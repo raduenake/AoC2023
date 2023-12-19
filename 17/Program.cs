1e888c5460e2827c23914632995fb74da9f1a5e7
Radu E <radu.enache@nagarro.com>
2023-12-19 13:18:30 +0200

    17,19

---
+++ 17/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Immutable;
+using System.Net.Security;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+var file = System.IO.File.OpenText("input.txt");
+var input = file.ReadToEnd()
+    .Split("\n")
+    .Where(l => !string.IsNullOrEmpty(l))
+    .SelectMany((l, i) =>
+        l.Select((c, j) => (pos: new Vector2(i, j), val: int.Parse("" + c)))
+    ).ToImmutableDictionary(kv => kv.pos, kv => kv.val);
+
+var degToRad = (float deg) => (float)(Math.PI / 180.0) * deg;
+var rotateDeg = (Vector2 vec, float deg) =>
+{
+    var tmp = Vector2.Transform(vec, Quaternion.CreateFromAxisAngle(Vector3.UnitZ, degToRad(deg)));
+    return new Vector2((int)Math.Round(tmp.X), (int)Math.Round(tmp.Y));
+};
+
+var x = rotateDeg(Vector2.UnitX, -90);
+
+var maxX = input.Keys.Max(k => k.X);
+var maxY = input.Keys.Max(k => k.Y);
+
+var moveDict = new[] { (Vector2.UnitX, "D"), ((-1) * Vector2.UnitX, "U"), (Vector2.UnitY, "R"), ((-1) * Vector2.UnitY, "L") }.ToDictionary(kv => kv.Item1, kv => kv.Item2);
+var hash = (Vector2 last, Vector2 prev, string hist) =>
+{
+    return last.GetHashCode() + prev.GetHashCode() + hist.GetHashCode();
+};
+
+var shortDist = (int maxSteps, int stepsInLine) => { return 0; };
+shortDist = (int maxSteps, int stepsInLine) =>
+{
+    var prioQueue = new PriorityQueue<(Vector2, Vector2, string), int>();
+    var visited = new HashSet<int>();
+
+    //start is "Free";
+    prioQueue.Enqueue((Vector2.Zero, Vector2.Zero, ""), (-1) * input[Vector2.Zero]);
+    (Vector2 last, Vector2 prev, string moveHistory) current;
+    var loss = 0;
+    while (prioQueue.TryDequeue(out current, out loss))
+    {
+        if (current.last.X == maxX && current.last.Y == maxY)
+        {
+            var lastSteps = current.moveHistory.TakeLast(stepsInLine);
+            if (lastSteps.Count() == stepsInLine && lastSteps.Take(stepsInLine - 1).All(s => s == lastSteps.Last()))
+                return loss + input[current.last];
+        }
+
+        var dirOfTravel = current.last != current.prev ? current.last - current.prev : Vector2.Zero;
+        var dirUnits = new[] { (increment: Vector2.Zero, move: "_") };
+        if (dirOfTravel == Vector2.Zero)
+        {
+            dirUnits = [
+                (increment: Vector2.UnitX, move: moveDict[Vector2.UnitX]),
+                (increment: Vector2.UnitY, move: moveDict[Vector2.UnitY])
+            ];
+        }
+        else
+        {
+            dirUnits = [
+                (increment: rotateDeg(dirOfTravel, -90), move: moveDict[rotateDeg(dirOfTravel, -90)]),
+                (increment: dirOfTravel, move: moveDict[dirOfTravel]),
+                (increment: rotateDeg(dirOfTravel, 90), move: moveDict[rotateDeg(dirOfTravel, 90)])
+            ];
+        }
+
+        foreach (var dirUnit in dirUnits)
+        {
+            var next = current.last + dirUnit.increment;
+            if (!input.ContainsKey(next))
+                continue;
+
+            var moveHistory = current.moveHistory + dirUnit.move;
+            var tmpMoveHistory = moveHistory.TakeLast(maxSteps + 1);
+            if (
+                (tmpMoveHistory.Count() == maxSteps + 1) || 
+                (tmpMoveHistory.Count() < stepsInLine && tmpMoveHistory.TakeLast(stepsInLine).All(s => s == tmpMoveHistory.TakeLast(stepsInLine).Last())) ||
+                (tmpMoveHistory.Count() > 1 && 
+                tmpMoveHistory.Last() != tmpMoveHistory.Take(tmpMoveHistory.Count() - 1).Last() &&
+                tmpMoveHistory.Take(tmpMoveHistory.Count() - 1).TakeLast(stepsInLine).Count() == stepsInLine &&
+                tmpMoveHistory.Take(tmpMoveHistory.Count() - 1).TakeLast(stepsInLine)
+                    .Take(stepsInLine - 1).Any(h => h != tmpMoveHistory.Take(tmpMoveHistory.Count() - 1).Last()))
+            )
+            
+            {
+                continue;
+            }
+
+            var nextLoss = loss + input[current.last];
+            var h = hash(next, current.last, string.Join("", tmpMoveHistory));
+
+            if (!visited.Contains(h))
+            {
+                visited.Add(h);
+                prioQueue.Enqueue((next, current.last, string.Join("", tmpMoveHistory)), nextLoss);
+            }
+        }
+    }
+    return 0;
+};
+
+// var p1 = shortDist(3);
+
+// Console.WriteLine($"P1: {p1}");
+
+var p1 = shortDist(10, 4);
+
+Console.WriteLine($"P1: {p1}");
---
+++ 19/Program.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+var file = System.IO.File.OpenText("input.txt");
+var input = file.ReadToEnd()
+    .Split("\n")
+    .ToImmutableList();
+
+var workflows = new Dictionary<string, List<(string destination, string rating, int ratingValue, string logicOp)>>();
+var ratings = new List<Dictionary<string, int>>();
+
+Action<string> addToWorkflow = line =>
+{
+    var r = new Regex("^(.*)\\{(.*)\\}$");
+    var matches = r.Match(line);
+    var id = matches.Groups[1].Value;
+    var ops = matches.Groups[2].Value.Split(",").Select(op =>
+    {
+        var r = (destination: "", rating: "", ratingValue: 0, logicOp: "");
+        if (op.Contains(":"))
+        {
+            var opSplit = op.Split(":");
+            var logic = "<";
+            if (opSplit[0].Contains(">"))
+            {
+                logic = ">";
+            }
+            var logicSplit = opSplit[0].Split(logic);
+            r = (destination: opSplit[1], rating: logicSplit[0], ratingValue: int.Parse(logicSplit[1]), logicOp: logic);
+        }
+        else
+        {
+            r = (destination: op, rating: "", ratingValue: 0, logicOp: "");
+        }
+        return r;
+    }).ToList();
+    workflows[id] = ops;
+};
+
+Action<string> addToRatings = line =>
+{
+    var r = new Regex("^\\{(.*)\\}$");
+    var matches = r.Match(line);
+    var ratingValues = matches.Groups[1].Value.Split(",").Select(v =>
+    {
+        var rSplit = v.Split("=");
+        return (rating: rSplit[0], ratingValue: int.Parse(rSplit[1]));
+    }).ToDictionary(kv => kv.rating, kv => kv.ratingValue);
+
+    ratings.Add(ratingValues);
+};
+
+var isWorkflow = true;
+foreach (var l in input)
+{
+    if (string.IsNullOrEmpty(l))
+    {
+        isWorkflow = false;
+        continue;
+    }
+    if (isWorkflow)
+    {
+        addToWorkflow(l);
+    }
+    else
+    {
+        addToRatings(l);
+    }
+}
+
+var p1 = 0;
+foreach (var ratingValue in ratings)
+{
+    var ratingOutcome = "A";
+    var currentSelector = "in";
+    while (true)
+    {
+        var current = workflows[currentSelector];
+        var nextSelector = string.Empty;
+
+        var currentOpsEnum = current.GetEnumerator();
+        while (currentOpsEnum.MoveNext())
+        {
+            var currentOp = currentOpsEnum.Current;
+
+            var opEvaluation = true;
+            if (!string.IsNullOrEmpty(currentOp.rating))
+            {
+                var value = ratingValue[currentOp.rating];
+                opEvaluation = currentOp.logicOp == ">" ? value > currentOp.ratingValue : value < currentOp.ratingValue;
+            }
+
+            if (opEvaluation == true)
+            {
+                nextSelector = currentOp.destination;
+                break;
+            }
+        }
+        if (!string.IsNullOrEmpty(nextSelector))
+        {
+            if (nextSelector == "A" || nextSelector == "R")
+            {
+                ratingOutcome = nextSelector;
+                break;
+            }
+            currentSelector = nextSelector;
+        }
+    }
+    p1 += ratingOutcome switch
+    {
+        "A" => ratingValue.Sum(v => v.Value),
+        _ => 0
+    };
+}
+
+Console.WriteLine($"P1: {p1}");
+
+Func<string, Dictionary<string, (int min, int max)>, ulong> rng = (currentS, ranges) => { return 0L; };
+rng = (currentSelector, ranges) =>
+{
+    if (currentSelector == "R")
+        return 0;
+
+    if (currentSelector == "A")
+        return ranges.Values.Aggregate(1UL, (acc, r) => acc * (ulong)(r.max - r.min + 1));
+
+    var current = workflows[currentSelector];
+    var result = 0UL;
+
+    foreach (var currentOp in current)
+    {
+        if (string.IsNullOrEmpty(currentOp.rating))
+        {
+            result += rng(currentOp.destination, ranges);
+        }
+        else
+        {
+            var (min, max) = ranges[currentOp.rating];
+            if (currentOp.logicOp == ">")
+            {
+                if (min > currentOp.ratingValue)
+                {
+                    result += rng(currentOp.destination, ranges);
+                    return result;
+                }
+
+                if (max > currentOp.ratingValue)
+                {
+                    var tmpRanges = new Dictionary<string, (int min, int max)>(ranges)
+                    {
+                        [currentOp.rating] = (currentOp.ratingValue + 1, max)
+                    };
+                    result += rng(currentOp.destination, tmpRanges);
+
+                    ranges[currentOp.rating] = (min, currentOp.ratingValue);
+                }
+            }
+            else
+            {
+                if (max < currentOp.ratingValue)
+                {
+                    result += rng(currentOp.destination, ranges);
+                    return result;
+                }
+
+                if (min < currentOp.ratingValue)
+                {
+                    var tmpRanges = new Dictionary<string, (int min, int max)>(ranges)
+                    {
+                        [currentOp.rating] = (min, currentOp.ratingValue - 1)
+                    };
+                    result += rng(currentOp.destination, tmpRanges);
+
+                    ranges[currentOp.rating] = (currentOp.ratingValue, max);
+                }
+            }
+        }
+    }
+    return result;
+};
+
+var accRanges = (new[] { "x", "m", "a", "s" }).ToDictionary(kv => kv, kv => (1, 4000));
+var p2 = rng("in", accRanges);
+
+Console.WriteLine($"P2: {p2}");
